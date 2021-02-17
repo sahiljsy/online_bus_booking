954e2c586b3042afed9ac9dc39b639605f305fcc
AakarshInamdar <19ceubs119@ddu.ac.in>
2021-02-17 17:25:38 +0530

    Setting modified

--- online_bus_booking/online_bus_booking/settings.py
+++ online_bus_booking/online_bus_booking/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'online_bus_booking.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
